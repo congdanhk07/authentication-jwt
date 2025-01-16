811db5190821ff827f9f33f12760d7029bad488d
Cong Danh <danh.tran@dision.tech>
2025-01-16 17:42:46 +0700

    feat: update provider jwt

--- api-base-jwt-axios-interceptors/src/providers/JwtProvider.js
+++ api-base-jwt-axios-interceptors/src/providers/JwtProvider.js
@@ -1,1 +1,32 @@
-// Example
+import JWT from 'jsonwebtoken'
+
+/*
+Function tạo mới một token cần 3 tham số đầu vào
+userInfo: Những thông tin muốn đính kèm vào token
+secrectSignature: Chữ kí bí mật (String ngẫu nhiên)
+tokenLife: Thời gian sống của token
+*/
+const generateToken = async (userInfo, secrectSignature, tokenLife) => {
+  try {
+    return JWT.sign(userInfo, secrectSignature, {
+      algorithm: 'HS256',
+      expiresIn: tokenLife
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+// Check token có hợp lệ hay không  -> Token đc tạo ra có đúng với secretSignature hay không
+const verifyToken = (token, secrectSignature) => {
+  try {
+    return JWT.verify(token, secrectSignature)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const JwtProvider = {
+  generateToken,
+  verifyToken
+}
